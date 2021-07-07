60146a2071609b468826379dffb48165a3c370ed
loubana alarabi <loubana@dconfig.com>
2021-07-08 02:31:31 +0300

    contact page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root 'pages#welcome'
   get '/pages/portfolio' => 'pages#portfolio'
   get '/pages/about' => 'pages#about'
+  get '/pages/contact' => 'pages#contact'
 end
